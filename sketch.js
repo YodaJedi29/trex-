f10d23ed6b5445c44be69d07c4e273406cbb5d86
YodaJedi29 <117858392+YodaJedi29@users.noreply.github.com>
2022-11-21 15:22:09 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+var trex ,trex_running;
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  trex = createSprite(100,150)
+  trex.addAnimation("correndo",trex_running);
+  
+}
+
+function draw(){
+  background("white")
+  drawSprites();
+}
